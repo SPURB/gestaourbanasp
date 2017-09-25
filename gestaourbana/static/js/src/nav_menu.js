a95959fd64cb50d825367aeaeb909fdda4654582
yubathom <yubathom@gmail.com>
2017-09-25 20:27:09 -0300

    Fixes #1 - Arruma parcialmente a navegação
    Ajustar toggle de subitens

--- gestaourbana/static/js/src/nav_menu.js
+++ gestaourbana/static/js/src/nav_menu.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 	var niveis = ['.primeiro_nivel','.segundo_nivel','.terceiro_nivel','.quarto_nivel'];
 
-	for (var i = 0; niveis.length>i; i++){
-		$(niveis[i]).click(function(){
-			if ($(this).children('ul').is(':hidden')){
-				$(this).children('ul').show(300);
-			}
-			else{
-				$(this).children('ul').hide(300);
-			}
-		});
+	// hide onload
+	for (var j = 0; niveis.length>j; j++){
+		$(niveis[j]).children('ul').addClass('nav_hide');
 	}
 
+	// toggle hide
+	for (var i = 0; niveis.length>i; i++){
+		$(niveis[i]).click(function(){
+			$(this).children('ul').toggleClass('nav_hide');
+		});
+	}
 });
-
