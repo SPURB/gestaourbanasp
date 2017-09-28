13b20e8ff13c9d1e9b0a2ae9aa6ed0e362c0c78d
yubathom <yubathom@gmail.com>
2017-09-28 19:35:20 -0300

    Fixex #1
    Toggle nos irmãos

--- gestaourbana/static/js/src/nav_menu.js
+++ gestaourbana/static/js/src/nav_menu.js
@@ -1,21 +1,15 @@
-jQuery(document).ready(function(){
-	var nav_menu = $('.nav-menu');
-	if( nav_menu.length > 0 ) {
-		nav_menu.each(function(){
-			var accordion = $(this);
-			//detecta alterações em input[type="checkbox"]
-
-			accordion.on('change', 'input[type="checkbox"]', function(){
-				var checkbox = $(this);
-				// console.log(checkbox.prop('checked'));
-				( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
-				// if (checkbox.prop('checked')){
-				// 	checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300);
-				// }
-				// else{
-				// 	checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
-				// }
+$(document).ready(function(){
+	if ($('#nav-menu')) {
+		$('.expansor').each(function(){
+			$(this).on('click', function() {
+				if ($(this).next('ul').hasClass('expanded')) {
+					$(this).next('ul').toggleClass('expanded').slideUp(300);
+				}
+				else {
+					$(this).next('ul').attr('style', 'display:none;').slideDown(300);
+					$(this).next('ul').toggleClass('expanded');
+				};
 			});
 		});
-	}
+	};
 });
