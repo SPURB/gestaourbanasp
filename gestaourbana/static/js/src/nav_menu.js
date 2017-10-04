14a387ce51a71a7cfca38954d0c83e2931b9878c
yubathom <yubathom@gmail.com>
2017-10-04 18:53:31 -0300

    Permite navigacao em menu por teclado

--- gestaourbana/static/js/src/nav_menu.js
+++ gestaourbana/static/js/src/nav_menu.js
@@ -9,32 +9,36 @@ $(document).ready(function(){
 			$(el).next('ul').toggleClass('expanded');
 		};
 	}
-
-	function indexLevel(){
-		return document.activeElement.tabIndex.toString().length;
-	}
-
 	$('.expansor').each(function(){
 		$(this).on('click', function() {
 			menuToggle(this);
 		});
 	});
-
-	//acessibility shift and enter key control
+	//navegação por tab, shift+tab e enter
 	$(document).keypress(function(e) {
-		if(e.which == 13) {
-			var index_number = document.activeElement.tabIndex;
-			var collpased_children = $(document.activeElement).next('ul').children('li');
-
+		if (e.which == 13) {
+			var active = document.activeElement;
+			var index_number = active.tabIndex;
+			var collpased_children = $(active).next('ul').children('li');
+			var level = index_number.toString();		 // 1000 - primeiro nível / 1100 - segundo nível / 1110 - terceiro nível
+			var level_count = level.split("0").length-1; //    3 - primeiro nível /    2 - segundo nível /    1 - terceiro nível
 			collpased_children.each(function() {
-				index_number = index_number+1;
+				switch (level_count) {
+					case 3: index_number = index_number+100; break;
+					case 2: index_number = index_number+10; break;
+					case 1: index_number = index_number+1; break;
+				}
 				$(this).attr('tabindex', index_number);
 			});
-			menuToggle($(document.activeElement));
+			if (active.nodeName!=="BUTTON"){ //ignora ação de enter em botão - sobrepõe-se a default
+				menuToggle($(active));
+			}
+			if (active.firstChild.localName == 'a') { //se é link redireciona para link
+				window.location.href = active.firstChild.href;
+			}
 		}
 	});
-
-	//menu toggle itself for screens smaller than 750px
+	//menu toggle para telas menores que 750px
 	$('button.menu_btn').click(function() {
 		$(this).children('span').each(function() {
 			$(this).toggleClass('open');
